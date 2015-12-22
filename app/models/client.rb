554d33d9c453d1380e55817c3ac9a2bc9a5d6180
Maiclear <maidaclaro@gmail.com>
2015-12-21 22:47:23 -0300

    stattus?
    q
    exit
    Gemfile

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,9 +1,16 @@
 class Client < ActiveRecord::Base
 
+  before_save :default_role
+
   validates :name, presence: true
   validates :lastname, presence: true
   validates :phone
   validates :address
 
   enum status: [:moroso, :aldia, :suspendido, :debaja]
+
+  def default_role
+    self.role ||= 1
+  end
+
 end
