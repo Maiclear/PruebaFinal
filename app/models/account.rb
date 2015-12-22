fb128b513f757fd2e556c7a2658f70486f8d7cdd
Maiclear <maidaclaro@gmail.com>
2015-12-21 22:52:47 -0300

    error corregido

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,9 +2,8 @@ class Account < ActiveRecord::Base
   has_many :transfers
   has_many :users, through: :transfer
 
-  validates :description, presence: true
+  validates :descrition, presence: true
   validates :amount, presence: true
   validates :client, presence: true
-  validates :date
   validates :balance, presence: true
 end
